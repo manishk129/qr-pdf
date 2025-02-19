0647a706f418ed763c6d88b97643959f2ae8ad55
manish <manishkumarpandit12@gmail.com>
2025-02-20 00:43:53 +0530

    Updated docs and version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="qr-pdf",                     
-    version="1.0.0",                       
+    version="1.0.1",                       
     description="Python package to generate severals QR codes at once in PDF format", 
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="manish",
     author_email="manishkumarpandit12@gmail.com",
-    url="https://github.com/manishkumar-hub/qr-pdf",
+    url="https://github.com/manishk129/qr-pdf",
     packages=find_packages(),              # Automatically find sub-packages
     classifiers=[
         "Programming Language :: Python :: 3",
